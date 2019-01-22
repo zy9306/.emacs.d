4d43e0958e3e769c16505295877c2a770845acb5
Illyasviel <zy99306@outlook.com>
2019-01-22 20:42:23 +0800

    delete dashboard

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,18 +1,19 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-(use-package dashboard
-  :ensure t
+;; todo not very useful, wait to delete it
+;; (use-package dashboard
+;;   :ensure t
   
-  :config
-  (dashboard-setup-startup-hook)
-  (setq dashboard-startup-banner 2)
-  (setq dashboard-items '((recents  . 10)
-                          (bookmarks . 10)
-                          ;; (projects . 5)
-                          (agenda . 10)
-                          ;; (registers . 5)
-                          ))
-  )
+;;   :config
+;;   (dashboard-setup-startup-hook)
+;;   (setq dashboard-startup-banner 2)
+;;   (setq dashboard-items '((recents  . 10)
+;;                           (bookmarks . 10)
+;;                           ;; (projects . 5)
+;;                           (agenda . 10)
+;;                           ;; (registers . 5)
+;;                           ))
+;;   )
 
 
 (use-package diminish
