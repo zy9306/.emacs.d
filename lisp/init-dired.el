7184328269d73090edaea35b407c8dc96b022ca5
Illyasviel <zy99306@outlook.com>
2019-03-04 13:16:56 +0800

    dired -al -h --group-directories-first --color=always

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -7,4 +7,12 @@
   (setq dired-subtree-line-prefix "        ")
   (define-key dired-mode-map (kbd "C-<return>") 'dired-subtree-toggle))
 
+
+;; Prefer g-prefixed coreutils version of standard utilities when available
+;; brew install coreutils for mac
+(let ((gls (executable-find "gls")))
+  (when gls (setq insert-directory-program gls)))
+
+(setq dired-listing-switches "-al -h --group-directories-first --color=always")
+
 (provide 'init-dired)
