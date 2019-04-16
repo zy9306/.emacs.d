157e1811eee6eb011da42d88562e54d803c37457
Illyasviel <zy99306@outlook.com>
2019-04-16 19:44:01 +0800

    change swiper keybind

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -19,7 +19,7 @@
   (setq ivy-height 20)
   
   ;; Ivy-based interface to standard commands
-  (global-set-key (kbd "C-s") 'swiper)
+  (global-set-key (kbd "C-c s") 'swiper)
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
