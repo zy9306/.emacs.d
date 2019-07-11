33d59f015558d89300f65668537c47b65fbe6d26
Illyasviel <zy99306@outlook.com>
2019-07-11 05:41:47 -0700

    display-buffer-alist

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
 (require 'init-python)
 (require 'init-go)
 (require 'init-additional-major-mode)
+(require 'init-display-buffer)
 
 ;; evil config
 (if (not (display-graphic-p))
---
+++ lisp/init-display-buffer.el
@@ -0,0 +1,32 @@
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+
+; need enhance
+;  try shackle?
+
+
+(add-to-list 'display-buffer-alist
+             '("\\*.*\\*"
+               (display-buffer-reuse-window
+                display-buffer-below-selected)
+               (split-window-sensibly)
+               (window-height   . 0.3)
+               (reusable-frames . visible)))
+
+
+(add-to-list 'display-buffer-alist
+             '("\\*Ilist\\*"
+               (imenu-list-display-buffer)))
+
+
+(add-to-list 'display-buffer-alist
+             '(".*magit.*"
+               (display-buffer-reuse-window display-buffer-same-window)))
+
+
+(add-to-list 'display-buffer-alist
+             '(".*ivy-occur.*"
+               (display-buffer-reuse-window display-buffer-same-window)))
+
+
+(provide 'init-display-buffer)
