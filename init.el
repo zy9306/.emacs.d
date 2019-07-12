8d790cbbfbcf296a3f1cc36a724b399795fe9eae
Illyasviel <zy99306@outlook.com>
2019-07-12 07:25:40 -0700

    change face

--- init.el
+++ init.el
@@ -30,7 +30,6 @@
 (require 'init-go)
 (require 'init-additional-major-mode)
 (require 'init-display-buffer)
-(require 'init-face)
 
 ;; evil config
 (if (not (display-graphic-p))
@@ -67,7 +66,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-line ((t (:background "pink"))))
+ '(mode-line ((t (:background "white" :foreground "grey20" :box (:line-width -1 :color "dim gray") :weight light))))
+ '(mode-line-inactive ((t (:background "white" :foreground "grey20" :box (:line-width -1 :color "gray") :weight light)))))
 
 ;; close debug when finally load
 (setq debug-on-error nil)
