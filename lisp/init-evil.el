ad4ee5062f4c368af83a13c8c6283b87841e54d9
Illyasviel <zy99306@outlook.com>
2019-08-29 07:31:59 -0700

    modify evil

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,58 +1,61 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
 
-;; (use-package use-package-chords
-;;   :ensure t
-;;   :config (key-chord-mode 1)
-;; )
+(use-package use-package-chords
+  :ensure key-chord
+  :ensure t
+  :config (key-chord-mode 1)
+)
 
 (use-package evil
   :ensure t
   :config
   (evil-mode 1)
-
-  (define-key evil-normal-state-map (kbd "C-c") 'evil-force-normal-state)
-  (define-key evil-visual-state-map (kbd "C-c") 'evil-change-to-previous-state)
-  (define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
-  (define-key evil-replace-state-map (kbd "C-c") 'evil-normal-state)
-
   (modify-syntax-entry ?_ "w")
   )
 
-(use-package evil-tutor
-  :ensure t)
-
 
 (use-package general
-  :ensure t)
+  :ensure t
+  :config
+  (general-imap "j"
+                (general-key-dispatch 'self-insert-command
+                  :timeout 0.25
+                  "j" 'evil-normal-state)))
 
 
 (general-evil-setup)
+
+
 (general-nmap
   :prefix "SPC"
-  "xf" 'counsel-find-file
-  "xd" 'dired
-  "x1" 'delete-other-windows
-  "x2" 'split-window-below
-  "x3" 'split-window-right
-  "x0" 'delete-window
-  "xo" 'other-window
-  "xb" 'switch-to-buffer
-  "x4f" 'find-file-other-window
-  "tf" 'toggle-frame-maximized
+  "f" 'counsel-find-file
+  "d" 'dired
+  "1" 'delete-other-windows
+  "2" 'split-window-below
+  "3" 'split-window-right
+  "0" 'delete-window
+  "o" 'other-window
+  "b" 'switch-to-buffer
+  "4f" 'find-file-other-window
   "aw" 'ace-window
   "as" 'ace-swap-window
-  "si" 'swiper
-  "cg" 'counsel-git
-  "cj" 'counsel-git-grep
-  "ck" 'counsel-git-ag)
-
-
-(general-nmap
-  :prefix ";"
-  "'" 'avy-goto-char-2
-  ";" 'avy-goto-char
+  "s" 'swiper
+  "pf" 'projectile-find-file
+  "pk" 'projectile-kill-buffers
+  "ps" 'projectile-ripgrep' ;; or projectile-ag
+  "pp" 'projectile-switch-project
+  ";" 'avy-goto-char-2
+  "'" 'avy-goto-char
   "l" 'avy-goto-line
-)
+  "e" 'er/expand-region
+  "r" 'query-replace
+  "R" 'query-replace-regexp
+  )
+
+
+;; (general-nmap
+;;   :prefix ";"
+;; )
 
 (provide 'init-evil)
