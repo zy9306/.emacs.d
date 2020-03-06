eb648d2e0a5388074911c707576f2c429edc2b24
Illyasviel <zy99306@outlook.com>
2020-03-06 10:34:33 +0800

    fix rename-current-buffer-file

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -267,22 +267,27 @@
 
 ;; ;;;;;;; rename current file and buffer ;;;;;;
 
-;; https://emacsredux.com/blog/2013/05/04/rename-file-and-buffer/
+;; https://stackoverflow.com/questions/384284/how-do-i-rename-an-open-file-in-emacs/16838442#16838442
 
-(defun rename-file-and-buffer ()
-  "Rename the current buffer and file it is visiting."
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
   (interactive)
-  (let ((filename (buffer-file-name)))
+  (let* ((name (buffer-name))
+        (filename (buffer-file-name))
+        (basename (file-name-nondirectory filename)))
     (if (not (and filename (file-exists-p filename)))
-        (message "Buffer is not visiting a file!")
-      (let ((new-name (read-file-name "New name: " filename)))
-        (cond
-         ((vc-backend filename) (vc-rename-file filename new-name))
-         (t
-          (rename-file filename new-name t)
-          (set-visited-file-name new-name t t)))))))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " (file-name-directory filename) basename nil basename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
 
-(global-set-key (kbd "C-c r")  #'rename-file-and-buffer)
+(global-set-key (kbd "C-c r")  #'rename-current-buffer-file)
 
 
 
