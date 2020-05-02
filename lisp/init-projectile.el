5fd249e04b06dd5ad42e1e9d1301f173138b52d9
Illyasviel <zy99306@outlook.com>
2020-05-02 19:02:55 +0800

    add neotree

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -14,4 +14,30 @@
   (setq projectile-mode-line-function '(lambda () (format " Proj[%s]" (projectile-project-name))))
 
 
+
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-dir)
+              (neotree-find file-name)))
+      (message "Could not find git project root."))))
+
+
+(use-package neotree
+  :ensure t
+  :defer t
+  :init
+  (global-set-key [f8] 'neotree-toggle)
+  (global-set-key (kbd "C-c C-p") 'neotree-project-dir)
+  :config
+  (setq neo-smart-open t)
+  (setq projectile-switch-project-action 'neotree-projectile-action))
+
+
 (provide 'init-projectile)
