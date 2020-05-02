a628d1612814e93d01922cf89f54fec20ef4d9c5
Illyasviel <zy99306@outlook.com>
2020-05-02 18:18:04 +0800

    adjust theme

--- init.el
+++ init.el
@@ -82,18 +82,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (spacemacs-dark)))
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages (quote (spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line-inactive ((t (:background "gray27" :foreground "#b2b2b2" :box (:line-width 1 :color "#5d4d7a")))))
- '(region ((t (:background "sea green" :foreground "black")))))
+ '(hl-line ((t (:background "pink"))))
+ '(mode-line ((t (:background "white" :foreground "grey20" :box (:line-width -1 :color "dim gray") :slant normal :weight light))))
+ '(mode-line-inactive ((t (:background "white" :foreground "grey20" :box (:line-width -1 :color "gray") :slant italic :weight light)))))
 
 
 ;; close debug when finally load
