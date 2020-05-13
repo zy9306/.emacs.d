2db2b81a7a914f210f8a07747686b2130f5e5149
Illyasviel <zy99306@outlook.com>
2020-05-13 22:48:18 +0800

    add lsp doc

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -5,7 +5,27 @@
 ;; https://emacs-lsp.github.io/lsp-mode/
 ;; https://github.com/emacs-lsp/lsp-ui
 
-;; lsp-auto-guess-root + remap xref能解决项目路径问题
+
+;; lsp-auto-guess-root为t时
+;; 通过.git等版本控制相关文件夹确定项目目录
+;; 也可通过.projectile文件确定项目目录
+;; 如果该目录不是一个项目目录，则lsp不会启动，也无法手动启动
+;; 不要将大目录作为项目目录，因为lsp会分析目录
+;; 可建立一个单独的带有.projectile的文件夹作为项目之外的编码使用
+
+;; 当lsp-auto-guess-root为nil时
+;; 每进入一个无法确定是项目目录的文件夹时会询问
+;; 可选择手动导入或者直接将当前目录作为项目目录（不推荐）
+;; 也可以永久忽悠该目录
+;; lsp-workspace-blacklist-remove可以移除忽略的目录
+;; 如果一个目录被忽略，其子目录也会被忽悠，即使新建了.projectile等文件
+
+;; 相关问题
+;; 当lsp-auto-guess-root为t时，以mspyls为例，M-.从项目目录查找
+;; 定义进入python虚拟环境目录时如果使用lsp-find-definition可能导致lsp
+;; 不在进入的文件启动M-.默认是xref-find-definitions，将其remap到
+;; lsp-find-definition即可解决问题
+
 
 
 (setq lsp-keymap-prefix "C-c l")
