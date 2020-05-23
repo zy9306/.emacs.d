1a8f67a1bef26f933aaca49e4ddae4f299edbaea
Illyasviel <zy99306@outlook.com>
2020-05-23 19:33:43 +0800

    scroll

--- lisp/init-edit-utils.el
+++ lisp/init-edit-utils.el
@@ -55,36 +55,11 @@
 (global-set-key (kbd "C-9") (lambda () (interactive) (previous-line 2)))
 (global-set-key (kbd "C-(") (lambda () (interactive) (scroll-down-line 2)))
 (global-set-key (kbd "C-)") (lambda () (interactive) (scroll-up-line 2)))
+(global-set-key (kbd "C-*") (lambda () (interactive) (scroll-left 1)))
+(global-set-key (kbd "C-&") (lambda () (interactive) (scroll-right 1)))
+(global-set-key (kbd "C-}") (lambda () (interactive) (scroll-left)))
+(global-set-key (kbd "C-{") (lambda () (interactive) (scroll-right)))
 
-
-(defun iscroll (&optional arg)
-  (interactive)
-  (or arg (setq arg 1))
-  (let (c)
-    (catch 'done
-      (while t
-	(message
-	 "k=up, j=down, h=left, l=right, n=scroll-down, p=scroll-up (by %d);  1-9=unit, q=quit"
-	 arg)
-	(setq c (read-char))
-	(condition-case ()
-	    (cond
-	     ((= c ?p) (scroll-down-line arg))
-	     ((= c ?n) (scroll-up-line arg))
-
-	     ((= c ?j) (next-line arg))
-	     ((= c ?k) (previous-line arg))
-         ((= c ?l) (forward-char))
-         ((= c ?h) (backward-char))
-
-	     ((= c ?\^G) (keyboard-quit))
-	     ((= c ?q) (throw 'done t))
-	     ((and (> c ?0) (<= c ?9)) (setq arg (- c ?0)))
-	     (t (beep)))
-	  (error (beep)))))
-    (message "Done.")))
-
-(global-set-key (kbd "C-*") 'iscroll)
 
 
 (use-package move-text
