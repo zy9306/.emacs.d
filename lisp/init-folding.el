7563fdf9f9b5b85147abc7593cd616ad3f483a2c
Illyasviel <zy99306@outlook.com>
2020-07-09 09:15:29 +0800

    disable origami

--- lisp/init-folding.el
+++ lisp/init-folding.el
@@ -1,11 +1,11 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
 
-;; (use-package yafolding
-;;   ;; C-RET: toggle-element  C-S-RET: hide-parent-element  C-M-RET: toggle-all
-;;   :ensure t
-;;   :config
-;;   (add-hook 'prog-mode-hook 'yafolding-mode))
+(use-package yafolding
+  ;; C-RET: toggle-element  C-S-RET: hide-parent-element  C-M-RET: toggle-all
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'yafolding-mode))
 
 
 (use-package vimish-fold
@@ -19,88 +19,90 @@
 
 ;; https://github.com/wbolster/dotfiles/blob/master/Emacs/init.el
 
-(use-package origami
-  :custom
-  (origami-show-fold-header t)
+;; 基本可以正常使用，但行为上有些怪异的问题，暂且不用
 
-  ;; :custom-face
-  ;; (origami-fold-replacement-face ((t (:inherit magit-diff-context-highlight))))
-  ;; (origami-fold-fringe-face ((t (:inherit magit-diff-context-highlight))))
+;; (use-package origami
+;;   :custom
+;;   (origami-show-fold-header t)
 
-  :commands
-  w--origami-parser-imenu-flat
+;;   ;; :custom-face
+;;   ;; (origami-fold-replacement-face ((t (:inherit magit-diff-context-highlight))))
+;;   ;; (origami-fold-fringe-face ((t (:inherit magit-diff-context-highlight))))
 
-  :config
-  (global-origami-mode t)
+;;   :commands
+;;   w--origami-parser-imenu-flat
 
-  (face-spec-reset-face 'origami-fold-header-face)
+;;   :config
+;;   (global-origami-mode t)
 
-  (defun w--origami-mode-toggle ()
-    (interactive)
-    (origami-mode 'toggle)
-    (when origami-mode
-      (if (> (point) 1)
-          (origami-show-only-node (current-buffer) (point))
-        (origami-close-all-nodes (current-buffer)))))
+;;   (face-spec-reset-face 'origami-fold-header-face)
 
-  (defun w--origami-parser-imenu-flat (create)
-    "Origami parser producing folds for each imenu entry, without nesting."
-    (lambda (content)
-      (let ((orig-major-mode major-mode))
-        (with-temp-buffer
-          (insert content)
-          (funcall orig-major-mode)
-          (let* ((items
-                  (-as-> (imenu--make-index-alist t) items
-                         (-flatten items)
-                         (-filter 'listp items)))
-                 (positions
-                  (-as-> (-map #'cdr items) positions
-                         (-filter 'identity positions)
-                         (-map-when 'markerp 'marker-position positions)
-                         (-filter 'natnump positions)
-                         (cons (point-min) positions)
-                         (-snoc positions (point-max))
-                         (-sort '< positions)
-                         (-uniq positions)))
-                 (ranges
-                  (-zip-pair positions (-map '1- (cdr positions))))
-                 (fold-nodes
-                  (--map
-                   (-let*
-                       (((range-beg . range-end) it)
-                        (line-beg
-                         (progn (goto-char range-beg)
-                                (line-beginning-position)))
-                        (offset
-                         (- (min (line-end-position) range-end) line-beg))
-                        (fold-node
-                         (funcall create line-beg range-end offset nil)))
-                     fold-node)
-                   ranges)))
-            fold-nodes)))))
+;;   (defun w--origami-mode-toggle ()
+;;     (interactive)
+;;     (origami-mode 'toggle)
+;;     (when origami-mode
+;;       (if (> (point) 1)
+;;           (origami-show-only-node (current-buffer) (point))
+;;         (origami-close-all-nodes (current-buffer)))))
 
-  (setq origami-parser-alist
-        '(
-         (java-mode . origami-java-parser)
-         (c-mode . origami-c-parser)
-         (c++-mode . origami-c-style-parser)
-         (perl-mode . origami-c-style-parser)
-         (cperl-mode . origami-c-style-parser)
-         (js-mode . origami-c-style-parser)
-         (js2-mode . origami-c-style-parser)
-         (js3-mode . origami-c-style-parser)
-         (go-mode . origami-c-style-parser)
-         (php-mode . origami-c-style-parser)
-         (python-mode . w--origami-parser-imenu-flat)
-         (emacs-lisp-mode . origami-elisp-parser)
-         (lisp-interaction-mode . origami-elisp-parser)
-         (clojure-mode . origami-clj-parser)
-         )
-        )
+;;   (defun w--origami-parser-imenu-flat (create)
+;;     "Origami parser producing folds for each imenu entry, without nesting."
+;;     (lambda (content)
+;;       (let ((orig-major-mode major-mode))
+;;         (with-temp-buffer
+;;           (insert content)
+;;           (funcall orig-major-mode)
+;;           (let* ((items
+;;                   (-as-> (imenu--make-index-alist t) items
+;;                          (-flatten items)
+;;                          (-filter 'listp items)))
+;;                  (positions
+;;                   (-as-> (-map #'cdr items) positions
+;;                          (-filter 'identity positions)
+;;                          (-map-when 'markerp 'marker-position positions)
+;;                          (-filter 'natnump positions)
+;;                          (cons (point-min) positions)
+;;                          (-snoc positions (point-max))
+;;                          (-sort '< positions)
+;;                          (-uniq positions)))
+;;                  (ranges
+;;                   (-zip-pair positions (-map '1- (cdr positions))))
+;;                  (fold-nodes
+;;                   (--map
+;;                    (-let*
+;;                        (((range-beg . range-end) it)
+;;                         (line-beg
+;;                          (progn (goto-char range-beg)
+;;                                 (line-beginning-position)))
+;;                         (offset
+;;                          (- (min (line-end-position) range-end) line-beg))
+;;                         (fold-node
+;;                          (funcall create line-beg range-end offset nil)))
+;;                      fold-node)
+;;                    ranges)))
+;;             fold-nodes)))))
 
-  (global-set-key (kbd "<C-return>") 'origami-toggle-node)
-  (global-set-key (kbd "<C-M-return>") 'origami-toggle-all-nodes))
+;;   (setq origami-parser-alist
+;;         '(
+;;          (java-mode . origami-java-parser)
+;;          (c-mode . origami-c-parser)
+;;          (c++-mode . origami-c-style-parser)
+;;          (perl-mode . origami-c-style-parser)
+;;          (cperl-mode . origami-c-style-parser)
+;;          (js-mode . origami-c-style-parser)
+;;          (js2-mode . origami-c-style-parser)
+;;          (js3-mode . origami-c-style-parser)
+;;          (go-mode . origami-c-style-parser)
+;;          (php-mode . origami-c-style-parser)
+;;          (python-mode . w--origami-parser-imenu-flat)
+;;          (emacs-lisp-mode . origami-elisp-parser)
+;;          (lisp-interaction-mode . origami-elisp-parser)
+;;          (clojure-mode . origami-clj-parser)
+;;          )
+;;         )
+
+;;   (global-set-key (kbd "<C-return>") 'origami-toggle-node)
+;;   (global-set-key (kbd "<C-M-return>") 'origami-toggle-all-nodes))
 
 
 (provide 'init-folding)
