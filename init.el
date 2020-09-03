02afc138cf79a3fd4679abad37ad45a5a959bc32
Illyasviel <zy99306@outlook.com>
2020-09-03 22:06:40 +0800

    adjust

--- init.el
+++ init.el
@@ -50,7 +50,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(general key-chord vimish-fold yafolding keyfreq shackle protobuf-mode dockerfile-mode yaml-mode yasnippet go-mode tide js2-mode flycheck-rust rust-mode flycheck-pycheckers yapfify auto-virtualenv pyvenv lsp-python-ms toc-org lsp-ivy lsp-ui lsp-mode which-key use-package symbol-overlay spinner smex rg real-auto-save rainbow-delimiters neotree multiple-cursors move-text markdown-mode magit ivy-prescient ivy-hydra imenu-list highlight-indent-guides goto-chg flycheck expand-region exec-path-from-shell dired-subtree diminish diff-hl counsel-projectile company-prescient buffer-move browse-kill-ring anzu ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
