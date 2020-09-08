5ff9243f4b1b5563cf36e9e0d51b6938f6ceb5ec
Illyasviel <zy99306@outlook.com>
2020-09-08 09:22:23 +0800

    add embrace

--- lisp/init-surround.el
+++ lisp/init-surround.el
@@ -1,11 +1,16 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
 
+;; https://github.com/cute-jumper/embrace.el
 ;; https://github.com/ganmacs/emacs-surround
 
+
+(require-package 'embrace)
 
 (require 'emacs-surround)
 
 (global-set-key (kbd "M-\"") 'emacs-surround)
 
-(provide 'init-emacs-surround)
+(global-set-key (kbd "M-]") #'embrace-commander)
+
+(provide 'init-surround)
