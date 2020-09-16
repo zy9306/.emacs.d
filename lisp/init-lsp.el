5d2770de8f1334af9696fa8bc437001f55203d89
Illyasviel <zy99306@outlook.com>
2020-09-16 09:18:00 +0800

    config lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -45,6 +45,38 @@
   (define-key lsp-mode-map [remap xref-find-definitions] #'lsp-find-definition)
   (define-key lsp-mode-map [remap xref-find-references] #'lsp-find-references)
 
+  (setq lsp-enable-file-watchers nil)
+
+  ;; http://blog.binchen.org/posts/how-to-speed-up-lsp-mode.html
+
+  ;; enable log only for debug
+  (setq lsp-log-io nil)
+
+  (setq lsp-enable-folding nil)
+
+  ;; no real time syntax check
+  (setq lsp-diagnostic-package :none)
+
+  (setq lsp-enable-snippet nil)
+
+  ;; turn off for better performance
+  (setq lsp-enable-symbol-highlighting nil)
+
+  ;; use ffip instead
+  (setq lsp-enable-links nil)
+
+  ;; auto restart lsp
+  (setq lsp-restart 'auto-restart)
+
+  ;; don't ping LSP lanaguage server too frequently
+  (defvar lsp-on-touch-time 0)
+  (defadvice lsp-on-change (around lsp-on-change-hack activate)
+    ;; don't run `lsp-on-change' too frequently
+    (when (> (- (float-time (current-time))
+                lsp-on-touch-time) 30) ;; 30 seconds
+      (setq lsp-on-touch-time (float-time (current-time)))
+      ad-do-it))
+
   (require 'lsp-ui)
   )
 
