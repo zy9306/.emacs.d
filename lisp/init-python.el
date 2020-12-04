09ea67cb05589f2f40c8dc71fb1868de0e36dc01
Illyasviel <zy99306@outlook.com>
2020-12-04 09:14:41 +0800

    use anaconda-mode in python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -40,37 +40,39 @@
   (modify-syntax-entry ?_ "w"))
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
-(defun mspyls-latest-nupkg-url ()
-  ;; https://github.com/emacs-lsp/lsp-python-ms/blob/master/lsp-python-ms.el#L172
-  ;; 进入python-mode时会自动下载mspyls，如果不能正常下载，手动获取url进行下载
-  ;; 将下载的文件解压到~/.emacs.d/.cache/lsp/mspyls/
-  ;; 二进制文件为~/.emacs.d/.cache/lsp/mspyls/Microsoft.Python.LanguageServer
-  ;; 确实Microsoft.Python.LanguageServer在unix环境下有执行权限
-  (interactive)
-  (lsp)
-  (message "%s" (lsp-python-ms-latest-nupkg-url "stable")))
+;; 下载 Microsoft.Python.LanguageServer
+;; nox 也有类似的函数
+;; (defun mspyls-latest-nupkg-url ()
+;;   ;; https://github.com/emacs-lsp/lsp-python-ms/blob/master/lsp-python-ms.el#L172
+;;   ;; 进入python-mode时会自动下载mspyls，如果不能正常下载，手动获取url进行下载
+;;   ;; 将下载的文件解压到~/.emacs.d/.cache/lsp/mspyls/
+;;   ;; 二进制文件为~/.emacs.d/.cache/lsp/mspyls/Microsoft.Python.LanguageServer
+;;   ;; 确实Microsoft.Python.LanguageServer在unix环境下有执行权限
+;;   (interactive)
+;;   (lsp)
+;;   (message "%s" (lsp-python-ms-latest-nupkg-url "stable")))
 
 
-;; anaconda-mode old config
+;; nox 补全不是很好，lsp-mode 太慢
 
-;; (use-package anaconda-mode
-;;   :ensure t
-;;   :defer t
-;;   :ensure company-anaconda
+(use-package anaconda-mode
+  :ensure t
+  :defer t
+  :ensure company-anaconda
 
-;;   :diminish anaconda-mode
+  :diminish anaconda-mode
 
-;;   :hook ((python-mode . anaconda-mode)
-;;          (python-mode . anaconda-eldoc-mode))
+  :hook ((python-mode . anaconda-mode)
+         )
 
-;;   :config
-;;   (require 'rx)
-;;   (eval-after-load "company"
-;;     '(add-to-list 'company-backends 'company-anaconda))
+  :config
+  (require 'rx)
+  (eval-after-load "company"
+    '(add-to-list 'company-backends 'company-anaconda))
 
-;;   (global-set-key (kbd "C-c M-r") 'anaconda-mode-find-references)
+  (global-set-key (kbd "C-c M-r") 'anaconda-mode-find-references)
 
-;;   (setq python-shell-interpreter "ipython"
-;;         python-shell-interpreter-args "-i --simple-prompt"))
+  (setq anaconda-eldoc-mode nil)
+  )
 
 (provide 'init-python)
