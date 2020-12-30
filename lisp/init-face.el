36d8413f892d173492ae934dce27601553015018
Illyasviel <zy99306@outlook.com>
2020-12-30 09:30:57 +0800

    add init-face

---
+++ lisp/init-face.el
@@ -0,0 +1,18 @@
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+;; mode-line
+(set-face-attribute
+ 'mode-line nil
+ :background "white" :foreground "grey20" :box '(:line-width -1 :color "dim gray") :slant 'normal :weight 'light)
+(set-face-attribute
+ 'mode-line-inactive nil
+ :background "white" :foreground "grey20" :box '(:line-width -1 :color "gray") :slant 'italic :weight 'light)
+
+(with-eval-after-load 'hl-line-mode
+  (set-face-attribute
+   'hl-line nil
+   :background "pink"))
+
+
+
+(provide 'init-face)
