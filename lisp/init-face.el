dbee89f0d1652b672f0dae028a804b474d3735b7
Illyasviel <zy99306@outlook.com>
2021-01-14 11:27:24 +0800

    update face

--- lisp/init-face.el
+++ lisp/init-face.el
@@ -7,6 +7,9 @@
 (set-face-attribute
  'mode-line-inactive nil
  :background "white" :foreground "grey20" :box '(:line-width -1 :color "gray") :slant 'italic :weight 'light)
+(set-face-attribute
+ 'cursor nil
+ :background miku-color)
 
 (with-eval-after-load 'hl-line-mode
   (set-face-attribute
--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -10,7 +10,7 @@
      (setq awesome-tray-active-modules
         '("location" "git" "mode-name" "parent-dir" "buffer-name" "battery" "date" "buffer-read-only"))
      (awesome-tray-mode 1))
-(add-hook 'after-init-hook #'local/awesome-tray-mode)
+;; (add-hook 'after-init-hook #'local/awesome-tray-mode)
 
 
 (provide 'init-theme)
