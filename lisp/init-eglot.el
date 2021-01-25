f44f930c129d4c1dd5ba6e2bddff69daa094b16c
Illyasviel <zy99306@outlook.com>
2021-01-25 18:31:52 +0800

    config eglot-server-programs

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -11,8 +11,16 @@
 
 
 
-;; start eglot 
+;; start eglot
+(defun local/eglot-server-programs ()
+  (require 'eglot)
+
+  (defvar local/eglot-pyright-langserver '(python-mode . ("pyright-langserver" "--stdio")))
+  (if (not (member local/eglot-pyright-langserver eglot-server-programs))
+      (add-to-list 'eglot-server-programs local/eglot-pyright-langserver)))
+
 (defun local/eglot-ensure ()
+  (local/eglot-server-programs)
   (setq eldoc-echo-area-use-multiline-p nil)
   (flycheck-mode -1)
   (eglot-ensure))
