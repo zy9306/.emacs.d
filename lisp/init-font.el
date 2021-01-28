9b97a357029867ff0c80a7bd23573f5f85095e0b
Illyasviel <zy99306@outlook.com>
2021-01-28 16:59:31 +0800

    NOTE: refactor init-font

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -1,57 +1,43 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-;; only work for english
-;; (if (display-graphic-p)
-;;     (progn
-;;       (set-face-attribute 'default nil :font
-;;                           "Source Code Pro 14")
-;;       (setq default-frame-alist
-;;             (append '((font . "Source Code Pro 14")) default-frame-alist))
-;;       ))
-
-
-;; fix `emacs --daemon` can't load font correctly.
 (if (daemonp)
+    ;; fix emacs --daemon
     (add-to-list 'default-frame-alist '(font . "Source Code Pro 15")))
 
-
-;; Setting English Font
+;; English Font
 (when *linux*
   (set-face-attribute 'default nil :font "Source Code Pro 11")
 )
-
 (when *win*
   (set-face-attribute 'default nil :font "Ubuntu Mono 13")
-)
 
-;; mac dpi is too high, so enlarge font
+  (set-default 'process-coding-system-alist
+               '(("[pP][lL][iI][nN][kK]" gbk-dos . gbk-dos)
+                 ("[cC][mM][dD][pP][rR][oO][xX][yY]" gbk-dos . gbk-dos))))
 (when *mac*
+  ;; mac dpi is too high, so enlarge font
   (set-face-attribute 'default nil :font "Source Code Pro 15")
 )
 
-;; Chinese Font
-;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
-;;   (set-fontset-font (frame-parameter nil 'font)
-;;                     charset (font-spec :family "思源黑体"
-;;                                        :size 16)))
-;; fix: 不设:size,而是改用倍数缩放,如果直接设置字号,则页面缩放时只能缩放英文
-;; 中文字体在-nw时都会报错
-(if (display-graphic-p)
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font)
-                        charset (font-spec :family "FZKai-Z03"))))
-(if (display-graphic-p)
-    ;; select a chinese font and type C-u C-x = what font used for chinese
+;; CJK Font
+(when (display-graphic-p)
+  ;; 中文字体在 -nw 时都会报错
+  ;; 不要在 font-spec 中设置倍数，而是改用倍数缩放,如果直接设置字号,则页
+  ;; 面缩放时只能缩放英文(font-spec :family "思源黑体" :size 16)
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset (font-spec :family "FZKai-Z03")))
+
+    ;; C-u C-x = 查看当前选中的字符所用的字体
     ;; 中英文等宽可调至1.2左右，1.0即为等高，等宽等高二选一
-    (setq face-font-rescale-alist '(("FZKai-Z03" . 1.0))))
+    ;; 调整倍数使以下中文和英文长度相等（等宽）
+    ;; 你你你你你你你你你你你你你你你你你你你你
+    ;; aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+  (setq face-font-rescale-alist '(("FZKai-Z03" . 1.0))))
 
-;; 调整倍数使以下中文和英文长度相等
-;; 你你你你你你你你你你你你你你你你你你你你
-;; aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
-
-;; 设置字符集
-;; 如果一个非 UTF-8 编码, 比如 GBK 编码的文件打开, 会乱码, 这时候 M-x revert-buffer-with-coding-system 选择 gbk 即可.
+;; 设置字符集，如果一个非 UTF-8 编码, 比如 GBK 编码的文件打开, 会乱码,
+;; 这时候 M-x revert-buffer-with-coding-system 选择 gbk 即可.
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
@@ -66,11 +52,6 @@
 
 (setq locale-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-
-(when *win*
-  (set-default 'process-coding-system-alist
-               '(("[pP][lL][iI][nN][kK]" gbk-dos . gbk-dos)
-                 ("[cC][mM][dD][pP][rR][oO][xX][yY]" gbk-dos . gbk-dos))))
 
 
 (provide 'init-font)
