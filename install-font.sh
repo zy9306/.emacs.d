795c6294cb06aa9358ffd1d663734b57b83d3cd4
Illyasviel <zy99306@outlook.com>
2021-02-01 11:29:08 +0800

    enhance install-font.sh

--- install-font.sh
+++ install-font.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-# FontDir=$HOME/.local/share/fonts
-FontDir=/usr/share/fonts/
+set -e
 
-mkdir -p $FontDir
+case "$(uname -s)" in
+    Linux)
+        # $HOME/.local/share/fonts
+        FONT_DIR=/usr/share/fonts/
+        ;;
+    Darwin)
+        # /usr/share/fonts/
+        FONT_DIR=$HOME/Library/Fonts
+        ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        echo 'Only Support Linux and OSX.'
+        exit 0
+        ;;
+    *)
+        echo 'Only Support Linux and OSX'
+        exit 0
+        ;;
+esac
 
-cp ./theme/all-the-icons.el/fonts/* $FontDir
+mkdir -p $FONT_DIR
+
+cp ./theme/all-the-icons.el/fonts/* $FONT_DIR
 
 fc-cache -vf
