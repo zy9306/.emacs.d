c74f565c042cce28c011ce8dae5be435d1a27058
Illyasviel <zy99306@outlook.com>
2021-02-05 12:38:20 +0800

    enhance search

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -6,6 +6,7 @@
   (require 'counsel)
   (require 'ivy-hydra)
   (require 'wgrep)
+
   (diminish 'ivy-mode)
   (diminish 'counsel-mode)
   (ivy-mode)
@@ -18,9 +19,10 @@
   ;; recentf 显示相对路径，而不是只显示文件名
   (setq ivy-virtual-abbreviate 'abbreviate)
 
-  (global-set-key (kbd "C-c s") 'swiper)
+  (global-set-key (kbd "C-s") 'swiper)
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
   (global-set-key (kbd "<f1> l") 'counsel-find-library)
