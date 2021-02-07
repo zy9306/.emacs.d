6706352cab16028208c3f3dded981035c71b3832
Illyasviel <zy99306@outlook.com>
2021-02-07 15:24:33 +0800

    local/go-test-current-func

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -29,4 +29,36 @@
   (add-hook 'before-save-hook 'gofmt-before-save))
 (add-hook 'go-mode-hook #'go-mode-save-hooks)
 
+(defun local/go-test-current-func ()
+  (interactive)
+  (let (current-test-name current-bench-name current-func-loc)
+    (save-excursion
+      (when (go-beginning-of-defun)
+        (setq current-func-loc (format "%s:%d" buffer-file-name (line-number-at-pos)))
+        (when (looking-at go-func-regexp)
+          (let ((func-name (match-string 1)))
+            (when (string-match-p "_test\.go$" buffer-file-name)
+              (cond
+               ((string-match-p "^Test\\|^Example" func-name)
+                (setq current-test-name func-name))
+               ((string-match-p "^Benchmark" func-name)
+                (setq current-bench-name func-name))))))))
+
+    (if current-func-loc
+        (let (go-test-buffer-name go-test-command)
+          (cond
+           (current-test-name
+            (setq go-test-buffer-name "*go-test*")
+            (setq go-test-command (concat "go" " test -- -test.run " current-test-name)))
+           (current-bench-name
+            (setq go-test-buffer-name "*go-test*")
+            (setq go-test-command (concat "go" " test -- -test.run='^$' -test.bench=" current-bench-name))))
+
+          (let ((go-test-buffer (get-buffer go-test-buffer-name)))
+            (when go-test-buffer (kill-buffer go-test-buffer)))
+          ;; or use start-process
+          (async-shell-command go-test-command go-test-buffer-name))
+      (error "Not in function"))))
+
+
 (provide 'init-go)
