76d5cf95669e6484d3015d66386f70934f644624
Illyasviel <zy99306@outlook.com>
2021-03-08 12:40:20 +0800

    fix mini-frame in mac

--- lisp/init-mini-frame.el
+++ lisp/init-mini-frame.el
@@ -2,9 +2,19 @@
 
 (defun local/mini-frame-mode ()
   (setq mini-frame-show-parameters
-        '((alpha . (70 70)) (left . 0.5) (top . 0.2) (width . 0.75) (height . 10)
-          (background-color . "#ffffff")))
+        '((top . 0.2)
+          (left . 0.5)
+          (width . 0.7)
+          (height . 10)
+          ;; linux xfce4 环境下无法法透明
+          (alpha . (90 90))))
+
+  ;; mac 下无法显示 border，背景色用默认的，白色的对比度太低
+  (when *linux*
+    (add-to-list 'mini-frame-show-parameters '(background-color . "#ffffff")))
+
   (setq mini-frame-internal-border-color "#39c5bb")
+
   ;; 固定高度为 10, 禁用自动调整，否则会闪烁
   (setq mini-frame-resize nil)
   (mini-frame-mode +1))
