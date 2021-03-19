5b07e4144b235521cdd3cf810e9b2d14f8e9bca8
Chtholly <zy99306@outlook.com>
2021-03-19 20:28:04 +0800

    adjust face

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(visible-mark selectrum consult mini-frame apheleia bufler git-auto-commit-mode project-root lsp-pyright nox lsp-mode quelpa ivy-posframe el-mock buttercup persp-mode deferred undercover ecukes espuds ert-runner pfuture evil-surround evil use-package-chords json-mode easy-kill restclient corral general key-chord vimish-fold yafolding keyfreq shackle protobuf-mode dockerfile-mode yaml-mode yasnippet go-mode tide js2-mode flycheck-rust rust-mode flycheck-pycheckers yapfify auto-virtualenv pyvenv lsp-python-ms toc-org lsp-ivy lsp-ui which-key use-package symbol-overlay spinner smex rg real-auto-save rainbow-delimiters neotree multiple-cursors move-text markdown-mode magit ivy-prescient ivy-hydra imenu-list highlight-indent-guides goto-chg flycheck expand-region exec-path-from-shell dired-subtree diminish diff-hl company-prescient buffer-move browse-kill-ring anzu ace-window))
+   '(spaceline visible-mark selectrum consult mini-frame apheleia bufler git-auto-commit-mode project-root lsp-pyright nox lsp-mode quelpa ivy-posframe el-mock buttercup persp-mode deferred undercover ecukes espuds ert-runner pfuture evil-surround evil use-package-chords json-mode easy-kill restclient corral general key-chord vimish-fold yafolding keyfreq shackle protobuf-mode dockerfile-mode yaml-mode yasnippet go-mode tide js2-mode flycheck-rust rust-mode flycheck-pycheckers yapfify auto-virtualenv pyvenv lsp-python-ms toc-org lsp-ivy lsp-ui which-key use-package symbol-overlay spinner smex rg real-auto-save rainbow-delimiters neotree multiple-cursors move-text markdown-mode magit ivy-prescient ivy-hydra imenu-list highlight-indent-guides goto-chg flycheck expand-region exec-path-from-shell dired-subtree diminish diff-hl company-prescient buffer-move browse-kill-ring anzu ace-window))
  '(safe-local-variable-values '((url-max-redirections . 0)))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
@@ -12,4 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "#e7e7e7" :box (:line-width 1 :color "grey75"))))))
