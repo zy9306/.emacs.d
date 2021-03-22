c615f0c90d8ad90cf2d879eea8e3b469575e16bd
Chtholly <zy99306@outlook.com>
2021-03-22 20:26:31 +0800

    display-buffer-use-some-window when hit RET

--- lisp/init-bufler.el
+++ lisp/init-bufler.el
@@ -4,11 +4,18 @@
 (global-set-key (kbd "C-x C-S-b") 'list-buffers)
 
 
+(defun local/bufler ()
+  (interactive)
+  (bufler)
+  (let ((buffer-window (get-buffer-window "*Bufler*")))
+    (set-window-parameter buffer-window 'no-delete-other-windows t)
+    (set-window-dedicated-p buffer-window t)))
+
 (with-eval-after-load 'bufler
-  (global-set-key [remap list-buffers] 'bufler)
+  (global-set-key [remap list-buffers] 'local/bufler)
 
   (with-eval-after-load 'key-chord
-    (key-chord-define-global "jk" 'bufler))
+    (key-chord-define-global "jk" 'local/bufler))
   )
 
 (local/after-init-hook 'bufler)
