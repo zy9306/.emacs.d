6fd2a49b7953fa317c99ba2986f00ad2924878b7
Illyasviel <zy99306@outlook.com>
2021-04-09 15:55:14 +0800

    adjust nox and eglot config

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,18 +1,24 @@
-;; -*- coding: utf-8; lexical-binding: t; -*-
+;;; -*- coding: utf-8; lexical-binding: t; -*-
 
 
-;; start hooks
-;; eglot
+;;; START HOOKS
+
+;; EGLOT
+
 ;; (add-hook 'go-mode-hook #'local/eglot-ensure)
-;; nox
+
+;; NOX
+
 (add-hook 'python-mode-hook #'local/nox-ensure)
 (add-hook 'rust-mode-hook #'local/nox-ensure)
 (add-hook 'go-mode-hook #'local/nox-ensure)
-;; end hooks 
+
+;; END HOOKS 
 
 
 
-;; start eglot
+;;; START EGLOT
+
 (defun local/eglot-server-programs ()
   (require 'eglot)
 
@@ -25,32 +31,36 @@
   (setq eldoc-echo-area-use-multiline-p nil)
   (flycheck-mode -1)
   (eglot-ensure))
-;; end eglot 
+
+;;; END EGLOT 
 
 
 
-;; start nox 
+;;; START NOX 
 ;; fork of eglot
 ;; https://github.com/manateelazycat/nox
 ;; https://manateelazycat.github.io/emacs/nox/2020/03/29/nox.html
+
+(setq nox-optimization-p nil)
+(setq nox-autoshutdown t)
+(add-hook 'pyvenv-post-activate-hooks
+          '(lambda ()
+             (setq nox-python-path (executable-find "python"))))
+
 (defun local/nox-ensure ()
-  ;; 自行设置 gc-cons-threshold，不通过 nox
-  (setq nox-optimization-p nil)
-  (setq nox-autoshutdown t)
-  ;; sudo npm install -g pyright
-  (setq nox-python-server "pyright")
-  (add-hook 'pyvenv-post-activate-hooks
-            '(lambda ()
-               (setq nox-python-path (executable-find "python"))))
   (nox-ensure)
   (local/config-company-backends))
 
-;; config mspyls
-;; 1. nox-print-mspyls-download-url
-;; 2. ~/.emacs.d/nox/mspyls/
-;; 3. 保证这个目录下面可以直接找到 Microsoft.Python.LanguageServer 这个文件
-;; (setq nox-python-server-dir "~/.emacs.d/.cache/lsp/mspyls/")
-;; end nox
+;; for pyright
+;; sudo npm install -g pyright
+(setq nox-python-server "pyright")
+
+;; for mspyls
+;; nox-print-mspyls-download-url and download then extract to nox-python-server-dir
+(setq nox-python-server-dir (expand-file-name ".cache/lsp/mspyls/" user-emacs-directory))
+
+
+;;; END NOX
 
 
 
