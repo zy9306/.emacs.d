fddaf5bd6a6be526efa1377e4b5c3f63c5cb2ff2
Chtholly <zy99306@outlook.com>
2021-08-04 11:21:32 +0800

    clean some code

--- lisp/init-option.el
+++ lisp/init-option.el
@@ -33,11 +33,6 @@
 (if (fboundp 'menu-bar-mode)
   (menu-bar-mode -1))
 
-;; set frame title to filename with short path
-;; (setq frame-title-format
-;;       '((:eval (if (buffer-file-name)
-;;                    (abbreviate-file-name (buffer-file-name))
-;;                  "%b"))))
 
 ;; 项目名 + 文件名
 (setq frame-title-format
