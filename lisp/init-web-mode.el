3d765cc9ee3090e9cec7fcd81d3b6a812998ff9a
Illyasviel <zy99306@outlook.com>
2021-08-24 12:56:19 +0800

    TODO web-mode

---
+++ lisp/init-web-mode.el
@@ -0,0 +1,60 @@
+;;; -*- coding: utf-8; lexical-binding: t; -*-
+
+(require-package 'tide)
+(require-package 'web-mode)
+
+
+(add-to-list 'auto-mode-alist '("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . js-mode))
+(add-to-list 'interpreter-mode-alist (cons "node" 'js2-mode))
+
+(add-hook 'js-mode-hook 'js2-minor-mode)
+
+(setq-default js2-basic-offset 2)
+
+
+;;; TSX
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+
+;;; JSX
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+
+;; "*HTTP Response*" buffer 不要启用tide
+(defun setup-tide-mode-patch ()
+    (if (not (equal (buffer-name) "*HTTP Response*"))
+        (setup-tide-mode)))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(add-hook 'js-mode-hook #'setup-tide-mode-patch)
+
+
+(provide 'init-web-mode)
