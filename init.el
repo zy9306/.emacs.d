781efbc370135fb92eb529c08a28139dabc55bc0
Illyasviel <zy99306@outlook.com>
2021-09-26 14:34:07 +0800

    evil when not display-graphic-p

--- init.el
+++ init.el
@@ -69,8 +69,8 @@
 
 (local/load-package 'init-keybinding)
 
-(when not (display-graphic-p)
-      (local/load-package 'init-evil-v3))
+(when (not (display-graphic-p))
+  (local/load-package 'init-evil-v3))
 
 
 ;; 自动检测编码，如果错误的将utf-8检测成gbk等中文编码，可能会导致lsp崩
