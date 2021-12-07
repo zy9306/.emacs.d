309705e57b97428979f3c14f55e0e4c4f9e9f10f
Illyasviel <zy99306@outlook.com>
2021-12-07 13:08:50 +0800

    theme/load.el

--- pkg.el
+++ pkg.el
@@ -1,8 +1,5 @@
 (load-file (expand-file-name "site-lisp/load.el" user-emacs-directory))
-
-(push (expand-file-name "theme/doom-themes" user-emacs-directory) load-path)
-(push (expand-file-name "theme/all-the-icons.el" user-emacs-directory) load-path)
-(push (expand-file-name "theme/spaceline" user-emacs-directory) load-path)
+(load-file (expand-file-name "theme/load.el" user-emacs-directory))
 
 (require-package 'quelpa)
 (setq quelpa-update-melpa-p nil)
---
+++ theme/load.el
@@ -0,0 +1,3 @@
+(push (expand-file-name "theme/doom-themes" user-emacs-directory) load-path)
+(push (expand-file-name "theme/all-the-icons.el" user-emacs-directory) load-path)
+(push (expand-file-name "theme/spaceline" user-emacs-directory) load-path)
