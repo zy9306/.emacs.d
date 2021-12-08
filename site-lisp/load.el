6c17f9c782a10029ef555180ed0c9b434d8b3ed6
Illyasviel <zy99306@outlook.com>
2021-12-08 16:18:13 +0800

    add thing-edit

--- site-lisp/load.el
+++ site-lisp/load.el
@@ -1,2 +1,3 @@
 (push (expand-file-name "site-lisp/color-rg" user-emacs-directory) load-path)
+(push (expand-file-name "site-lisp/thing-edit" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/toggle-one-window" user-emacs-directory) load-path)
