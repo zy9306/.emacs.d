8c2316b96aded43af45732c833e7f98e4aaaaffb
Illyasviel <zy99306@outlook.com>
2021-12-09 13:03:33 +0800

    add ivy-posframe

--- site-lisp/load.el
+++ site-lisp/load.el
@@ -1,6 +1,7 @@
 (push (expand-file-name "site-lisp/color-rg" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/duplicate-line" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/hydra-posframe" user-emacs-directory) load-path)
+(push (expand-file-name "site-lisp/ivy-posframe" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/one-key" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/posframe" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/thing-edit" user-emacs-directory) load-path)
