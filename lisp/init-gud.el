c6ffddb34621c505264ba1cf44ad3818e2730b59
Illyasviel <zy99306@outlook.com>
2021-12-13 17:37:59 +0800

    gud

--- lisp/init-gud.el
+++ lisp/init-gud.el
@@ -1,26 +1,27 @@
-;; -*- coding: utf-8; lexical-binding: t; -*-
+;;; -*- coding: utf-8; lexical-binding: t; -*-
 
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Debuggers.html
 
 
-(defhydra hydra-gud-menu (:color pink :hint nil)
-  ("b" gud-break "break")
-  ("d" gud-remove "remove")
-  ("n" gud-next "next")
-  ("s" gud-step "step")
-  ("p" gud-print "print")
-  ("t" gud-trace "trace")
-  ("r" gud-cont "continue")
-  ("f" gud-finish "finish")
-  ("l" gud-refresh "refresh")
-  ("<" gud-up "up")
-  (">" gud-down "down")
+(pretty-hydra-define local/gud (:foreign-keys warn :title "GUD" :quit-key "q")
+  ("KEY"
+   (("b" gud-break "break")
+    ("d" gud-remove "remove")
+    ("n" gud-next "next")
+    ("s" gud-step "step")
+    ("p" gud-print "print")
+    ("t" gud-trace "trace")
+    ("r" gud-cont "continue")
+    ("f" gud-finish "finish")
+    ("l" gud-refresh "refresh")
+    ("<" gud-up "up")
+    (">" gud-down "down")
 
-  ("G" dlv-current-func "dlv-current-func")
+    ("G" dlv-current-func "dlv-current-func"))))
 
-  ("q" quit-window "quit" :color blue))
 
-(global-set-key (kbd "C-x C-a h") 'hydra-gud-menu/body)
+(global-set-key (kbd "C-x C-a h") 'local/gud)
+
 
 (with-eval-after-load 'go-mode
   (require 'go-dlv))
