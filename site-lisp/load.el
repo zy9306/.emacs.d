aeccbdb0c78dab4b188c486e6f9e8f811964fa6c
Chtholly <zy99306@outlook.com>
2021-12-15 19:16:24 +0800

    delete centaur-tabs

--- site-lisp/load.el
+++ site-lisp/load.el
@@ -1,5 +1,4 @@
 (push (expand-file-name "site-lisp/awesome-tab" user-emacs-directory) load-path)
-(push (expand-file-name "site-lisp/centaur-tabs" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/color-rg" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/duplicate-line" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/emacs-mini-frame" user-emacs-directory) load-path)
