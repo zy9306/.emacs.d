8c12a6844b939933357b7b6278c43a7b8a85f72f
Chtholly <zy99306@outlook.com>
2021-12-18 18:40:52 +0800

    edit

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -51,40 +51,184 @@
     ))
 
 
-(require 'general)
+
 (require 'thing-edit)
 (require 'emacs-surround)
 (require 'duplicate-line)
 
 
+(defun local/thing-edit-internal (object-beg object-end &optional kill-conditional)
+  (interactive)
+  (let ((pulse-iterations 1)
+        (pulse-delay thing-edit-flash-line-delay))
+    (cond ((string= kill-conditional "cut")
+           (when thing-edit-show-message-p
+             (message "%s [ %s ]"
+                      (propertize "Cut" 'face 'thing-edit-font-lock-action)
+                      (buffer-substring object-beg object-end)))
+           (kill-region object-beg object-end))
+          ((string= kill-conditional "delete")
+           (when thing-edit-show-message-p
+             (message "%s [ %s ]"
+                      (propertize "Cut" 'face 'thing-edit-font-lock-action)
+                      (buffer-substring object-beg object-end)))
+           (delete-region object-beg object-end))
+          ((string= kill-conditional "copy")
+           (when thing-edit-show-message-p
+             (message "%s [ %s ]"
+                      (propertize "Copy" 'face 'thing-edit-font-lock-action)
+                      (buffer-substring object-beg object-end)))
+           (pulse-momentary-highlight-region object-beg object-end 'thing-edit-font-lock-flash)
+           (kill-ring-save object-beg object-end))
+          (t
+           (message "Nothing to do.")))))
+
+
+(defun local/inside-pairs (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-inside-pairs)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+(defun local/outside-pairs (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-outside-pairs)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+(defun local/inside-quotes (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-inside-quotes)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+(defun local/outside-quotes (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-outside-quotes)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+(defun local/word (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-word)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+(defun local/symbol (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-symbol)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+(defun local/defun (kill-conditional)
+  (save-excursion)
+  (let ((cur_point (point))
+        (start)
+        (end))
+    (er/mark-defun)
+    (setq start (region-beginning))
+    (setq end (region-end))
+    (local/thing-edit-internal start end kill-conditional)
+    (if (string= kill-conditional "copy")
+        (goto-char cur_point))))
+
+
 (global-unset-key (kbd "C-\\"))
 
 (defhydra hydra-awesome-tab (global-map "C-\\" :exit t)
-  ("yw" thing-copy-word)
+  ;; er copy
+  ("yaw" (lambda () (interactive) (local/word "copy")))
+  ("ys" (lambda () (interactive) (local/symbol "copy")))
+  ("yf" (lambda () (interactive) (local/defun "copy")))
+
+  ("yip" (lambda () (interactive) (local/inside-pairs "copy")))
+  ("yap" (lambda () (interactive) (local/outside-pairs "copy")))
+
+  ("yi\"" (lambda () (interactive) (local/inside-quotes "copy")))
+  ("ya\"" (lambda () (interactive) (local/outside-quotes "copy")))
+
+  ;; er cut
+  ("daw" (lambda () (interactive) (local/word "cut")))
+  ("ds" (lambda () (interactive) (local/symbol "cut")))
+  ("df" (lambda () (interactive) (local/defun "cut")))
+
+  ("dip" (lambda () (interactive) (local/inside-pairs "cut")))
+  ("dap" (lambda () (interactive) (local/outside-pairs "cut")))
+
+  ("di\"" (lambda () (interactive) (local/inside-quotes "cut")))
+  ("da\"" (lambda () (interactive) (local/outside-quotes "cut")))
+
+  ;; er delete
+  ("Daw" (lambda () (interactive) (local/word "delete")))
+  ("Ds" (lambda () (interactive) (local/symbol "delete")))
+  ("Df" (lambda () (interactive) (local/defun "delete")))
+
+  ("Dip" (lambda () (interactive) (local/inside-pairs "delete")))
+  ("Dap" (lambda () (interactive) (local/outside-pairs "delete")))
+
+  ("Di\"" (lambda () (interactive) (local/inside-quotes "delete")))
+  ("Da\"" (lambda () (interactive) (local/outside-quotes "delete")))
+
+  ;; thing at point
   ("yp" thing-copy-parentheses)
   ("yn" thing-copy-number)
-  ("ys" thing-copy-symbol)
   ("yy" thing-copy-line)
-  ("ya" thing-copy-to-line-beginning)
-  ("ye" thing-copy-to-line-end)
+  ("yla" thing-copy-to-line-beginning)
+  ("yle" thing-copy-to-line-end)
 
-  ("dw" thing-cut-word)
   ("dp" thing-cut-parentheses)
   ("dn" thing-cut-number)
-  ("ds" thing-cut-symbol)
   ("dd" thing-cut-line)
-  ("da" thing-cut-to-line-beginning)
-  ("de" thing-cut-to-line-end)
+  ("dla" thing-cut-to-line-beginning)
+  ("dle" thing-cut-to-line-end)
 
   ("rw" thing-replace-word)
   ("rp" thing-replace-parentheses)
   ("rs" thing-replace-symbol)
 
+  ;; duplicate-line
   ("pp" duplicate-line-or-region-above :exit nil)
   ("nn" duplicate-line-or-region-below :exit nil)
   ("PP" duplicate-line-above-comment :exit nil)
   ("NN" duplicate-line-below-comment :exit nil)
 
+  ;; surround
   ("'" 'embrace-commander)
   ("\"" 'emacs-surround))
 
