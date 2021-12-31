7a032f10fac0626213a1ac4241443fbb0425a1e0
Illyasviel <zy99306@outlook.com>
2021-12-31 17:44:35 +0800

    fetch tree-sitter

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -1,38 +1,37 @@
 ;;; -*- coding: utf-8; lexical-binding: t; -*-
 
 
-;; https://github.com/ubolonton/tree-sitter-langs/releases/tag/0.10.1
 ;; ensure BUNDLE-VERSION exists!
 (when *linux*
-  (setq local/tree-sitter-lang-dir (expand-file-name "tree-sitter/langs/linux/" user-emacs-directory)))
+  (setq local/tree-sitter-lang-dir (expand-file-name "tree-sitter/src/langs/linux/" user-emacs-directory)))
 
 (when *win*
-  (setq local/tree-sitter-lang-dir (expand-file-name "tree-sitter/langs/windows/" user-emacs-directory)))
+  (setq local/tree-sitter-lang-dir (expand-file-name "tree-sitter/src/langs/windows/" user-emacs-directory)))
 
 (when *mac*
-  (setq local/tree-sitter-lang-dir (expand-file-name "tree-sitter/langs/darwin/" user-emacs-directory)))
+  (setq local/tree-sitter-lang-dir (expand-file-name "tree-sitter/src/langs/macos/" user-emacs-directory)))
 
 
 (setq tree-sitter-langs--dir local/tree-sitter-lang-dir)
 (setq tree-sitter-langs-grammar-dir tree-sitter-langs--dir)
 
 
-;; https://github.com/ubolonton/emacs-tree-sitter/releases/tag/0.15.1
 ;; ensure DYN-VERSION exists!
-(setq tsc--dir (expand-file-name "tree-sitter/tsc-dyn/" user-emacs-directory))
+(setq tsc--dir (expand-file-name "tree-sitter/src/dyn/" user-emacs-directory))
 (setq tsc-dyn-dir tsc--dir)
 
 
-(when (file-directory-p (expand-file-name "tree-sitter/" user-emacs-directory))
-  (require-package 'tree-sitter)
-  (require-package 'tree-sitter-langs)
+(push (expand-file-name "tree-sitter/src/elisp-tree-sitter" user-emacs-directory) load-path)
+(push (expand-file-name "tree-sitter/src/elisp-tree-sitter/core" user-emacs-directory) load-path)
+(push (expand-file-name "tree-sitter/src/tree-sitter-langs" user-emacs-directory) load-path)
 
+
+(when (file-directory-p (expand-file-name "tree-sitter/src/" user-emacs-directory))
   (require 'tree-sitter)
   (require 'tree-sitter-langs)
 
   (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-  )
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 
 (provide 'init-tree-sitter)
