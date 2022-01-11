26c187a0e32ee13d8650825c7293396ca7c453b3
Illyasviel <zy99306@outlook.com>
2022-01-11 12:57:08 +0800

    add buffer-file-coding-system-face in awesome-tray

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -2,6 +2,17 @@
 (with-eval-after-load 'awesome-tray
   (require 'awesome-tray)
 
+  (defun local/buffer-file-coding-system ()
+    (format "%s" buffer-file-coding-system))
+
+  (defface local/buffer-file-coding-system-face
+    '((t (:italic t)))
+    "awesome-tray coding-system face."
+    :group 'awesome-tray)
+
+  (add-to-list 'awesome-tray-module-alist
+               '("coding-system" . (local/buffer-file-coding-system local/buffer-file-coding-system-face)))
+
   (awesome-tray-mode 1)
 
   ;; see awesome-tray-module-alist
@@ -13,6 +24,7 @@
           "buffer-read-only"
           "mode-name"
           "git"
+          "coding-system"
           "battery"
           "date")))
 (local/after-init-hook 'awesome-tray)
@@ -29,38 +41,38 @@
   ;; override spaceline--theme
   (defun spaceline--theme (left second-left &rest additional-segments)
     (spaceline-compile
-      `(,left
-        auto-compile
-        ,second-left
-        (which-function :priority 99)
-        (major-mode :priority 79)
-        (process :when active)
-        ((flycheck-error flycheck-warning flycheck-info)
-         :when active
-         :priority 89)
-        (minor-modes :when active
-                     :priority 9)
-        (mu4e-alert-segment :when active)
-        (erc-track :when active)
-        (version-control :when active
-                         :priority 78)
-        (org-pomodoro :when active)
-        (org-clock :when active)
-        nyan-cat)
-      `((python-pyvenv :fallback python-pyenv)
-        (purpose :priority 94)
-        (battery :when active)
-        (selection-info :priority 95)
-        input-method
-        ((buffer-encoding-abbrev
-          point-position
-          line-column)
-         :separator " | "
-         :priority 96)
-        (global :when active)
-        ,@additional-segments
-        (buffer-position :priority 99)
-        (hud :priority 99)))
+     `(,left
+       auto-compile
+       ,second-left
+       (which-function :priority 99)
+       (major-mode :priority 79)
+       (process :when active)
+       ((flycheck-error flycheck-warning flycheck-info)
+        :when active
+        :priority 89)
+       (minor-modes :when active
+                    :priority 9)
+       (mu4e-alert-segment :when active)
+       (erc-track :when active)
+       (version-control :when active
+                        :priority 78)
+       (org-pomodoro :when active)
+       (org-clock :when active)
+       nyan-cat)
+     `((python-pyvenv :fallback python-pyenv)
+       (purpose :priority 94)
+       (battery :when active)
+       (selection-info :priority 95)
+       input-method
+       ((buffer-encoding-abbrev
+         point-position
+         line-column)
+        :separator " | "
+        :priority 96)
+       (global :when active)
+       ,@additional-segments
+       (buffer-position :priority 99)
+       (hud :priority 99)))
 
     (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
 
