905c7dec2f505c096b2dbe36774988747c71d596
Illyasviel <zy99306@outlook.com>
2022-03-14 11:32:40 +0800

    delete some org config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,6 @@
 ;;; -*- coding: utf-8; lexical-binding: t; -*-
 
-;; hook
+;; HOOK
 (with-eval-after-load 'org
   ;; face
   (require 'init-org-face)
@@ -10,8 +10,7 @@
 
   ;; https://github.com/tonyaldon/org-bars
   (require 'org-bars)
-  (add-hook 'org-mode-hook #'org-bars-mode)
-  )
+  (add-hook 'org-mode-hook #'org-bars-mode))
 
 
 ;; https://orgmode.org/manual/Dynamic-Headline-Numbering.html
@@ -53,28 +52,23 @@
 ;; org-show-block-all展开所有代码块
 (add-hook 'org-mode-hook 'org-hide-block-all)
 
+;; 导出时不要执行代码块
+(setq org-export-babel-evaluate nil)
 
 ;; like C-j
 (with-eval-after-load 'org
   (define-key org-mode-map (kbd "<RET>") 'org-return-indent))
 
-
 (setq org-todo-keywords
-      '((sequence
-         "TODO(t)"
-         "CONTINUE(c)"
-         "DONE(d)"
-         )))
+      '((sequence "TODO(t)" "CONTINUE(c)" "DONE(d)")))
 
 (setq org-todo-keyword-faces
       '(("TODO" . "black")
         ("CONTINUE" . "pink")
-        ("DONE" . "purple")
-        ))
+        ("DONE" . "purple")))
 
-;; 显示时间格式为 2019-01-25 Fri 14:55 ，若不设，星期会显示为中文
+;; 显示时间格式为 2019-01-25 Fri 14:55
 (setq system-time-locale "C")
-
 
 (use-package toc-org
   :defer t
@@ -82,7 +76,7 @@
          (markdown-mode . toc-org-mode)))
 
 
-;;; babel
+;;; BABEL
 ;; https://orgmode.org/manual/Results-of-Evaluation.html
 ;; https://orgmode.org/manual/Environment-of-a-Code-Block.html
 ;; 所有带有 :session py 的代码块都会在同一进程中执行，否则每个代码块都
@@ -95,19 +89,11 @@
    'org-babel-load-languages
    '((emacs-lisp . t)
      (python . t)
-     (shell . t))
-   )
+     (shell . t)))
 
   (setq org-babel-default-header-args:shell '((:results . "output") (:session . "sh"))
         org-babel-default-header-args:python '((:results . "output") (:session . "py"))
-        org-babel-default-header-args:emacs-lisp '((:results . "output") (:session . "elisp"))
-        ))
-
-
-;;; Capture
-;; http://www.zmonster.me/2018/02/28/org-mode-capture.html
-;; https://orgmode.org/manual/Capture-templates.html
-;; see C-h v org-capture-templates for more info
+        org-babel-default-header-args:emacs-lisp '((:results . "output") (:session . "elisp"))))
 
 (when *win*
   (setq local/cloud-dir
@@ -117,11 +103,9 @@
 (when (or *linux* *mac*)
   (setq local/cloud-dir "~/OneDrive"))
 
-(let (
-      (task (format "%s/Illyasviel/task.org" local/cloud-dir))
+(let ((task (format "%s/Illyasviel/task.org" local/cloud-dir))
       (Illyasviel (format "%s/Illyasviel/Illyasviel.org" local/cloud-dir))
-      (journal (format "%s/Illyasviel/journal.org" local/cloud-dir))
-      )
+      (journal (format "%s/Illyasviel/journal.org" local/cloud-dir)))
   (setq org-capture-templates
         `(
           ("t" "Todo" entry (file ,task)
@@ -132,8 +116,7 @@
 
           ("j" "Journal" entry (file+datetree ,journal)
            "* %?\nEntered on %U\n  %i\n  %a")
-          )
-        ))
+          )))
 
 (define-key global-map (kbd "C-c c t")
   (lambda () (interactive) (org-capture nil "t")))
@@ -141,7 +124,7 @@
   (lambda () (interactive) (org-capture nil "c")))
 
 
-;;; org-download
+;;; ORG-DOWNLOAD
 ;; https://github.com/abo-abo/org-download/tree/master
 (with-eval-after-load 'org
   (require 'org-download)
@@ -156,91 +139,27 @@
   (put 'org-download-image-dir 'safe-local-variable #'stringp))
 
 
-;;; read_only start
-;; 标题加 read_only 标签，使该标签下的所有内容变成只读
-;; (defun org-mark-all-readonly ()
-;;   (interactive)
-;;   ;; 先强制移除所有只读状态
-;;   (org-remove-all-readonly)
-;;   (let ((buf-mod (buffer-modified-p)))
-;;     (org-map-entries
-;;      (lambda ()
-;;        (let* ((element (org-element-at-point))
-;;               (begin (org-element-property :begin element))
-;;               (end (org-element-property :end element)))
-;;          (add-text-properties begin (- end 1) '(read-only t))))
-;;      "read_only")
-;;     (unless buf-mod
-;;       (set-buffer-modified-p nil)))
-;;   (message "Mark all read only!"))
-
-;; ;; 移除所有 read_only tag 只读状态
-;; (defun org-remove-all-readonly ()
-;;   (interactive)
-;;   (let ((buf-mod (buffer-modified-p)))
-;;     (org-map-entries
-;;      (lambda ()
-;;        (let* ((element (org-element-at-point))
-;;               (begin (org-element-property :begin element))
-;;               (end (org-element-property :end element))
-;;               (inhibit-read-only t))
-;;          (remove-text-properties begin (- end 1) '(read-only t))))
-;;      "read_only")
-;;     (unless buf-mod
-;;       (set-buffer-modified-p nil)))
-;;   (message "Cancel all read only!"))
-
-;; ;; 只移除当前光标所在区块的只读状态
-;; (defun org-remove-readonly ()
-;;   (interactive)
-;;   (let ((buf-mod (buffer-modified-p)))
-;;     (let* ((element (org-element-at-point))
-;;            (begin (org-element-property :begin element))
-;;            (end (org-element-property :end element))
-;;            (inhibit-read-only t))
-;;       (remove-text-properties begin (- end 1) '(read-only t)))
-;;     (unless buf-mod
-;;       (set-buffer-modified-p nil)))
-;;   (message "Cancel current read only!"))
-
-;; (add-hook 'org-mode-hook #'org-mark-all-readonly)
-;;; read_only end
-
-
-;;; formatter start
+;;; FORMATTER START
 (require 'cl-lib)
 
-(defvar org-blank-lines-after-heading 1
-  "Number of blank lines to separate a heading from the content.")
+(defvar org-blank-lines-after-heading 1)
 
-(defvar org-blank-lines-after-content (cons 2 4)
-  "Cons cell for the number of blank lines after content in a heading.
-The car is for when the next heading is at the same level, and
-the cdr is for when the next heading is at a different level.
-This is for the body specific to the headline, not counting
-subheadings.")
+(defvar org-blank-lines-after-content (cons 2 4))
 
 (defun org-format-heading-blank-lines ()
-  "Make sure each headline has exactly
-`org-blank-lines-after-heading' after the heading, and
-`org-blank-lines-after-content' blank lines at the end of its
-content. Only works when point is in a headline."
   (interactive)
   (when (org-at-heading-p)
     (let ((current-level (nth 0 (org-heading-components)))
           next-level)
       (save-excursion
         (org-end-of-meta-data)
-        ;; chomp blank lines then add what you want back.
         (while (and (not (eobp)) (looking-at "^[[:space:]]*$"))
           (kill-line))
         (insert (cl-loop for i from 0 below org-blank-lines-after-heading concat "\n")))
 
-      ;; Now go to the end of content and insert lines if needed.
       (save-excursion
         (when (outline-next-heading)
           (setq next-level (nth 0 (org-heading-components)))
-          ;; chomp lines back then reinsert them.
           (previous-line)
           (while (looking-at "^[[:space:]]*$")
             (kill-line)
@@ -255,15 +174,15 @@ content. Only works when point is in a headline."
   (interactive "P")
   (save-excursion
     (org-save-outline-visibility t
-      (org-cycle '(64))
-      (message nil)
-      (goto-char (point-min))
-      (while (re-search-forward org-heading-regexp nil t)
-        (org-format-heading-blank-lines)))))
-;;; formatter end
+                                 (org-cycle '(64))
+                                 (message nil)
+                                 (goto-char (point-min))
+                                 (while (re-search-forward org-heading-regexp nil t)
+                                   (org-format-heading-blank-lines)))))
+;;; FORMATTER END
 
 
-;;; hide src block
+;;; HIDE SRC BLOCK
 (defvar local/org-blocks-toggle-flag nil)
 
 (defun local/org-toggle-blocks ()
@@ -279,11 +198,9 @@ content. Only works when point is in a headline."
 
 
 
-;;; hugo
+;;; HUGO
 (with-eval-after-load 'ox
   (require 'ox-hugo))
 
-;; 导出时不要执行代码块
-(setq org-export-babel-evaluate nil)
 
 (provide 'init-org)
