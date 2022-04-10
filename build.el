54bed5caa08e158f3a08aacfbce36b03345e1ab7
Chtholly <zy99306@outlook.com>
2022-04-10 15:14:09 +0800

    disable byte compile

--- build.el
+++ build.el
@@ -1,16 +1,8 @@
 ;;; M-x `eval-buffer' here
 
-;; put `((emacs-lisp-mode . ((no-byte-compile . t))))' in
-;; `.dir-locals.el' if don't want to compile
-
-;; find . -type f -name "*.elc" | xargs -I {} rm -f {}
-
 (let ((elpa "~/.emacs.d/elpa")
       (lisp "~/.emacs.d/lisp")
       (site-lisp "~/.emacs.d/site-lisp"))
-  (byte-recompile-directory site-lisp 0 nil)
-  (byte-recompile-directory elpa 0 nil)
-  (byte-recompile-directory lisp 0 nil)
 
   (if (and (fboundp 'native-comp-available-p)
            (native-comp-available-p))
