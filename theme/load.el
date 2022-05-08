beb266201b58368ec460f024abef055892bf69c0
Chtholly <zy99306@outlook.com>
2022-05-08 17:01:25 +0800

    fix load path

--- theme/load.el
+++ theme/load.el
@@ -1,3 +1,4 @@
 (push (expand-file-name "theme/doom-themes" user-emacs-directory) load-path)
+(push (expand-file-name "theme/doom-themes/extensions" user-emacs-directory) load-path)
 (push (expand-file-name "theme/all-the-icons.el" user-emacs-directory) load-path)
 (push (expand-file-name "theme/spaceline" user-emacs-directory) load-path)
