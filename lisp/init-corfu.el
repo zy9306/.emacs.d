3f41f6b7410e2b1fdf7a08806fcf7208aa87b215
Chtholly <zy99306@outlook.com>
2022-05-10 21:53:43 +0800

    add corfu

---
+++ lisp/init-corfu.el
@@ -0,0 +1,50 @@
+;;; -*- coding: utf-8; lexical-binding: t; -*-
+
+(defun local/setup-corfu ()
+  (use-package corfu
+    :custom
+    (corfu-auto t)
+    (corfu-auto-prefix 1)
+    (corfu-separator ?\s)
+    (corfu-quit-at-boundary nil)
+    (corfu-quit-no-match t)
+    (corfu-preview-current 'insert)
+    (corfu-preselect-first t)
+    (corfu-on-exact-match nil)
+    (corfu-echo-documentation nil)
+    (corfu-scroll-margin 5)
+    :bind
+    (:map corfu-map
+          ("RET" . corfu-insert)
+          ("TAB" . corfu-next)
+          ("S-TAB" . corfu-previous))
+    :init
+    (global-corfu-mode))
+
+  (use-package orderless
+    :init
+    (setq completion-styles '(orderless basic))
+    (setq completion-category-defaults nil)
+    (setq completion-category-overrides '((file (styles . (partial-completion))))))
+
+  (add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer)
+
+  (use-package dabbrev
+    :bind (("M-/" . dabbrev-completion)
+           ("C-M-/" . dabbrev-expand))
+    :custom
+    (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'"))))
+
+(defun corfu-enable-in-minibuffer ()
+  (when (where-is-internal #'completion-at-point (list (current-local-map)))
+    (corfu-mode 1)))
+
+
+(with-eval-after-load 'corfu
+  (local/setup-corfu))
+
+
+(local/after-init-hook 'corfu)
+
+
+(provide 'init-corfu)
