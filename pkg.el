70519c88ecf19bfe946f3974b5f81ede05c95d7b
Illyasviel <zy99306@outlook.com>
2022-05-11 11:48:38 +0800

    add load

--- pkg.el
+++ pkg.el
@@ -1,5 +1,6 @@
 (load-file (expand-file-name "site-lisp/load.el" user-emacs-directory))
 (load-file (expand-file-name "theme/load.el" user-emacs-directory))
+(load-file (expand-file-name "repo/load.el" user-emacs-directory))
 
 (require-package 'paradox)
 (add-hook 'after-init-hook 'paradox-enable)
---
+++ repo/load.el
@@ -0,0 +1,1 @@
+(push (expand-file-name "lsp-bridge" user-emacs-directory) load-path)
