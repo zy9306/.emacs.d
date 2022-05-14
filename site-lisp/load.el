e32504e8b4c1215bb62d9ce0f7746402b07f7fcb
Chtholly <zy99306@outlook.com>
2022-05-14 14:02:16 +0800

    add tabnine-capf

--- site-lisp/load.el
+++ site-lisp/load.el
@@ -15,6 +15,7 @@
 (push (expand-file-name "site-lisp/org-bars" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/ox-hugo" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/posframe" user-emacs-directory) load-path)
+(push (expand-file-name "site-lisp/tabnine-capf" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/thing-edit" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/toggle-one-window" user-emacs-directory) load-path)
 (push (expand-file-name "site-lisp/vertico" user-emacs-directory) load-path)
