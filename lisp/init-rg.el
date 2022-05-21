764760a7d590f1de6e96cccc2f59ac5732d678dc
Chtholly <zy99306@outlook.com>
2022-05-21 13:15:12 +0800

    adjust rg

--- lisp/init-rg.el
+++ lisp/init-rg.el
@@ -1,38 +1,38 @@
-;; -*- coding: utf-8; lexical-binding: t; -*-
+;;; -*- coding: utf-8; lexical-binding: t; -*-
 
 ;; https://github.com/dajva/rg.el
 
-
-(with-eval-after-load 'rg
+(use-package rg
+  :init
   (setq rg-show-header t)
   (setq rg-group-result nil)
   (setq rg-command-line-flags '("-z"))
   (setq rg-buffer-name 'my-rg-buffer-name)
 
-  (define-key rg-mode-map "I" #'rg-rerun-toggle-ignore)
-  (define-key rg-mode-map (kbd "z") 'rg-occur-hide-lines-matching)
-  (define-key rg-mode-map (kbd "/") 'rg-occur-hide-lines-not-matching)
+  :config
+  (define-key rg-mode-map (kbd "z") #'rg-occur-hide-lines-matching)
+  (define-key rg-mode-map (kbd "/") #'rg-occur-hide-lines-not-matching)
+
+  (rg-define-search rg-project-all
+    :files "all"
+    :dir project)
 
   (rg-define-search search-everything-at-home
-    "Search files including hidden in home directory"
     :query ask
     :format literal
-    :files "everything"
+    :files "all"
     :flags ("--hidden")
     :dir (getenv "HOME")
     :menu ("Search" "h" "Home"))
-  )
 
-(global-set-key (kbd "C-c C-s") #'rg-menu)
-(local/after-init-hook 'rg)
+  (global-set-key (kbd "C-c C-s") #'rg-menu))
 
 (defun my-rg-buffer-name ()
   (let ((p (project-current)))
     (if p
- (format "rg %s" (abbreviate-file-name (cdr p)))
+        (format "rg %s" (abbreviate-file-name (cdr p)))
       "rg")))
 
-;;;###autoload
 (defun rg-occur-hide-lines-not-matching (search-text)
   "Hide lines that don't match the specified regexp."
   (interactive "MHide lines not matched by regexp: ")
@@ -49,7 +49,6 @@
           (when (not (looking-at-p "^\nrg finished "))
             (delete-region (point) (1+ (point-at-eol)))))))))
 
-;;;###autoload
 (defun rg-occur-hide-lines-matching  (search-text)
   "Hide lines matching the specified regexp."
   (interactive "MHide lines matching regexp: ")
