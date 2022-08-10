a311dc6225da521e77c4062b6dfb23706f584840
Illyasviel <zy99306@outlook.com>
2022-08-10 13:02:32 +0800

    disable python-mode lsp-bridge

--- lisp/init-lsp-bridge.el
+++ lisp/init-lsp-bridge.el
@@ -67,29 +67,29 @@
 
 
 ;;; python virtual env
-(defun local/lsp-bridge-get-single-lang-server-by-project (project-path filepath)
-  (let* ((json-object-type 'plist)
-         (custom-dir (expand-file-name ".cache/lsp-bridge/pyright" user-emacs-directory))
-         (custom-config (expand-file-name "pyright.json" custom-dir))
-         (default-config (json-read-file (expand-file-name "repo/lsp-bridge/langserver/pyright.json" user-emacs-directory)))
-         (settings (plist-get default-config :settings)))
+;; (defun local/lsp-bridge-get-single-lang-server-by-project (project-path filepath)
+;;   (let* ((json-object-type 'plist)
+;;          (custom-dir (expand-file-name ".cache/lsp-bridge/pyright" user-emacs-directory))
+;;          (custom-config (expand-file-name "pyright.json" custom-dir))
+;;          (default-config (json-read-file (expand-file-name "repo/lsp-bridge/langserver/pyright.json" user-emacs-directory)))
+;;          (settings (plist-get default-config :settings)))
 
-    (plist-put settings :pythonPath (executable-find "python"))
+;;     (plist-put settings :pythonPath (executable-find "python"))
 
-    (plist-put settings :python.analysis (plist-put (plist-get settings :python.analysis) :autoImportCompletions json-false))
+;;     (plist-put settings :python.analysis (plist-put (plist-get settings :python.analysis) :autoImportCompletions json-false))
 
-    (make-directory (file-name-directory custom-config) t)
+;;     (make-directory (file-name-directory custom-config) t)
 
-    (with-temp-file custom-config
-      (insert (json-encode default-config)))
+;;     (with-temp-file custom-config
+;;       (insert (json-encode default-config)))
 
-    custom-config))
+;;     custom-config))
 
-(add-hook 'python-mode-hook (lambda () (setq-local lsp-bridge-get-single-lang-server-by-project 'local/lsp-bridge-get-single-lang-server-by-project)))
+;; (add-hook 'python-mode-hook (lambda () (setq-local lsp-bridge-get-single-lang-server-by-project 'local/lsp-bridge-get-single-lang-server-by-project)))
 
-(add-hook 'pyvenv-post-activate-hooks
-          (lambda ()
-            (lsp-bridge-restart-process)))
+;; (add-hook 'pyvenv-post-activate-hooks
+;;           (lambda ()
+;;             (lsp-bridge-restart-process)))
 
 
 ;;; eglot
