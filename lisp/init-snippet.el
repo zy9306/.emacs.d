d37fdc363c942b099338007ba4de648f8af40eab
Illyasviel <zy99306@outlook.com>
2022-09-15 12:52:06 +0800

    adjust

--- lisp/init-snippet.el
+++ lisp/init-snippet.el
@@ -1,21 +1,13 @@
-;; -*- coding: utf-8; lexical-binding: t; -*-
+;;; -*- coding: utf-8; lexical-binding: t; -*-
 
-(defun local/yas-mode ()
-  (require 'yasnippet)
+(lazy-load-global-keys
+ '(("C-x j" . yas-insert-snippet))
+ "yasnippet")
+
+(with-eval-after-load 'yasnippet
+  (require 'yasnippet-snippets)
   (yas-minor-mode)
   (yas-global-mode)
   (diminish 'yas-minor-mode))
-
-(use-package yasnippet
-  :ensure t
-  :defer t
-  :init
-  (add-hook 'after-init-hook #'local/yas-mode))
-
-
-(use-package yasnippet-snippets)
-
-(global-set-key (kbd "C-x j") 'yas-insert-snippet)
-
 
 (provide 'init-snippet)
