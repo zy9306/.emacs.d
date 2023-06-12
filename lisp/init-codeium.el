2bde2a569a62e81fb172462c4fe1504fdcd68b24
Illyasviel <zy99306@outlook.com>
2023-06-12 15:34:42 +0800

    disable codeium

--- lisp/init-codeium.el
+++ lisp/init-codeium.el
@@ -1,8 +1,8 @@
-(use-package codeium
-  :init
-  (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
-  (add-hook 'emacs-startup-hook (lambda () (run-with-timer 0.5 nil #'codeium-init)))
-  )
+;; (use-package codeium
+;;   :init
+;;   (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
+;;   (add-hook 'emacs-startup-hook (lambda () (run-with-timer 0.5 nil #'codeium-init)))
+;;   )
 
 
 (provide 'init-codeium)
