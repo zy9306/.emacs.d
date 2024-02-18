10bbfa76f7e15298cbfe406694b40e3ec868480f
Illyasviel <zy99306@outlook.com>
2024-02-18 17:14:41 +0800

    tmp

--- submodules/load.el
+++
@@ -1,3 +0,0 @@
-(push (expand-file-name "submodules/lsp-bridge" user-emacs-directory) load-path)
-(push (expand-file-name "submodules/posframe-plus" user-emacs-directory) load-path)
-(push (expand-file-name "submodules/treesitter-context.el" user-emacs-directory) load-path)
